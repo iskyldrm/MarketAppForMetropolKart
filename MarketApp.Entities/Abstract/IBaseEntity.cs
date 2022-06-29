830a4714fe5417c83a85e35de0fb587453abecf7
isak yıldırım <isakyildirim@outlook.com>
2022-06-29 12:02:44 +0300

    Add project files.

---
+++ MarketApp.Entities/Abstract/IBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp.Entities.Abstract
+{
+    public interface IBaseEntity<T>
+    {
+        public T Id { get; set; }
+        public DateTime CreatTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+    }
+}
---
+++ MarketApp.Entities/Concrete/Products.cs
@@ -0,0 +1,44 @@
+﻿using MarketApp.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp.Entities.Concrete
+{
+    public class Products : IBaseEntity<int>,IEntity
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 40 Karakter uzunluğunda olacak şekilde girilmesi zorunlu alan.
+        /// </summary>
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; }
+
+        public int? SupplierID { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        [StringLength(20)]
+        public string QuantityPerUnit { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal? UnitPrice { get; set; }
+
+        public short? UnitsInStock { get; set; }
+
+        public short? UnitsOnOrder { get; set; }
+
+        public short? ReorderLevel { get; set; }
+
+        public bool Discontinued { get; set; }
+
+        public virtual Category Category { get; set; }
+        public DateTime CreatTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}
