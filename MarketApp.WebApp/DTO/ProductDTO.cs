315767805f0df1480f4b1b1d15cdfe18acd46894
isak yıldırım <isakyildirim@outlook.com>
2022-06-30 13:55:47 +0300

    API servisi örnek projesi başarılyla çalıştırıldı

---
+++ MarketApp.WebApp/DTO/ProductDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace MarketApp.WebApp.DTO
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string? SKU { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string? ProductDescirption { get; set; }
+
+        public string? SupplierID { get; set; }
+
+        public string? CategoryID { get; set; }
+
+        public string? TaxId { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        public decimal? MSRP { get; set; }
+
+        public short? UnitsInStock { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ MarketApp.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using MarketApp.WebApp.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Text.Json;
+
+namespace MarketApp.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public List<ProductDTO> ProductsWeb { get; set; }
+
+        public void OnGet()
+        {
+            ProductsWeb = new List<ProductDTO>();   
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create($"https://localhost:7210/api/Product");
+            httpWebRequest.Method = "GET";
+
+            string Products = string.Empty;
+            using (HttpWebResponse response1 = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                Stream stream = response1.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                Products = reader.ReadToEnd();
+                reader.Close();
+                stream.Close();
+
+            }
+            var products = JsonSerializer.Deserialize<List<ProductDTO>>(Products, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            foreach (var item in products)
+            {
+                ProductDTO productDTO = new ProductDTO();
+                productDTO.SKU = item.SKU;
+                productDTO.ProductName = item.ProductName;
+                productDTO.Discontinued = item.Discontinued;
+                ProductsWeb.Add(productDTO);
+            }
+        }
+    }
+}
